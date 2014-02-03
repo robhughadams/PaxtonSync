68f5a32c916a85fef84f63bef4f546a1de1ab2e6
Robert Adams <robhughadams@hotmail.com>
2014-02-03 01:13:49 +0000

    Initial Commit

---
+++ MembershipDetails.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaxtonSync
+{
+    public class MembershipDetails
+    {
+        //{\"becNumber\":\"1416\",\"firstName\":\"Robert\",\"lastName\":\"Adams\",\"membershipStatus\":\"Current\"}
+        
+        public int BecNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public MembershipStatus MembershipStatus { get; set; }
+    }
+
+    public enum MembershipStatus
+    {
+        Current,
+        LYBNTY,
+        Expired,
+        Cancelled,
+        Deceased
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net.Http.Headers;
+using PaxtonSync.Properties;
+
+namespace PaxtonSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fetching membership details from www.bec-cave.org.uk");
+            var membershipDetails = _DownloadMembershipDetails();
+
+            foreach (var details in membershipDetails)
+               Console.WriteLine("{0} {1} {2}", details.BecNumber, details.FirstName, details.LastName);
+
+
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static IReadOnlyCollection<MembershipDetails> _DownloadMembershipDetails()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Basic", Convert.ToBase64String(
+                        Encoding.ASCII.GetBytes(String.Format("{0}:{1}", Settings.Default.UserName, Settings.Default.Password))));
+
+                var responseStream = client.GetStreamAsync(Settings.Default.DownloadUrl).Result;
+                using (var reader = new JsonTextReader(new StreamReader(responseStream)))
+                {
+                    var jsonReader = new JsonSerializer();
+                    return jsonReader.Deserialize<IReadOnlyCollection<MembershipDetails>>(reader);
+                }
+            }
+        }
+    }
+}
